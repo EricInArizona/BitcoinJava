974e770f19e2839357738c9435613de911321832
Eric Chauvin <76618581+EricInArizona@users.noreply.github.com>
2021-03-22 05:22:02 -0700

    Create MainApp.java

---
+++ MainApp.java
@@ -0,0 +1,94 @@
+// Copyright Eric Chauvin 2019 - 2021.
+
+
+
+// Bitcoin in Java
+
+
+
+import javax.swing.SwingUtilities;
+
+
+
+public class MainApp implements Runnable
+  {
+  public static final String versionDate =
+                                    "3/22/2021";
+
+  private MainWindow mainWin;
+  // public ConfigureFile mainConfigFile;
+  private String[] argsArray;
+
+
+
+  public static void main( String[] args )
+    {
+    MainApp mApp = new MainApp( args );
+    SwingUtilities.invokeLater( mApp );
+    }
+
+
+
+  @Override
+  public void run()
+    {
+    setupProgram();
+    }
+
+
+
+  private MainApp()
+    {
+    }
+
+
+  public MainApp( String[] args )
+    {
+    argsArray = args;
+    }
+
+
+
+  private void setupProgram()
+    {
+    // checkSingleInstance()
+
+    /*
+    String programDirectory = "\\Eric\\Main\\Bitcoin\\Java\\";
+    int length = argsArray.length;
+    if( length > 0 )
+      programDirectory = argsArray[0];
+
+    String mainConfigFileName = programDirectory +
+                                      "MainConfigure.txt";
+
+    mainConfigFile = new ConfigureFile( this,
+                                mainConfigFileName );
+    */
+
+    mainWin = new MainWindow( this, "Bitcoin Java" );
+    mainWin.initialize();
+
+    /*
+    showStatus( " " );
+    showStatus( "argsArray length: " + length );
+    for( int count = 0; count < length; count++ )
+      showStatus( argsArray[count] );
+    */
+
+    // showStatus( " " );
+    }
+
+
+
+  public void showStatusAsync( String toShow )
+    {
+    if( mainWin == null )
+      return;
+
+    mainWin.showStatusAsync( toShow );
+    }
+
+
+
+  }
